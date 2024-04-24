df3f6ce0ee91d69b8b8a7fe937c68eddf1bcf6eb
Lauped <166504614+Lauped@users.noreply.github.com>
2024-04-24 14:35:05 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from sqlalchemy.orm import relationship
-import datetime
+from datetime import datetime, timedelta  # Importer datetime og timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:gokstad@localhost/gruppe_5_bibliotek'
@@ -20,13 +20,24 @@ class User(UserMixin, db.Model):
     fornavn = db.Column('Fornavn', db.String(100), nullable=False)
     etternavn = db.Column('Etternavn', db.String(100), nullable=False)
     email = db.Column('Email', db.String(100), unique=True, nullable=False)
-    password_hash = db.Column('password_hash', db.String(128))
+    password_hash = db.Column('password_hash', db.String(128), nullable=False)
+    role = db.Column('Role', db.String(10), default='user')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+class BokRating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bok_isbn = db.Column(db.BigInteger, db.ForeignKey('bøker.ISBN'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('studenter.StudentID'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    bruker = relationship('User', back_populates='ratings')
+    bok = relationship('Bøker', back_populates='ratings')
 
 
 @login_manager.user_loader
@@ -36,8 +47,7 @@ def load_user(user_id):
 
 @app.route("/")
 def home():
-    time_of_day = get_time_of_day()
-    return render_template('home.html', time_of_day=time_of_day)
+    return render_template('home.html')
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -65,7 +75,7 @@ def register():
 
 
 def get_time_of_day():
-    now = datetime.datetime.now()
+    now = datetime.now()
     current_hour = now.hour
 
     if current_hour < 12:
@@ -97,8 +107,19 @@ def login():
 @app.route('/profile')
 @login_required
 def profile():
-    time_of_day = get_time_of_day()
-    return render_template('profile.html', fornavn=current_user.fornavn, etternavn=current_user.etternavn, time_of_day=time_of_day)
+    today = datetime.now().date()
+    overdue_loans = LånteBøker.query.filter(
+        LånteBøker.StudentID == current_user.id,
+        LånteBøker.Levert == False,
+        LånteBøker.LånDato <= (today - timedelta(days=30))
+    ).all()
+
+    if overdue_loans:
+        for loan in overdue_loans:
+            flash(f'Bok med ISBN {loan.ISBN} må returneres umiddelbart!', 'warning')
+    greeting = get_time_of_day()
+    return render_template('profile.html', time_of_day=greeting, fornavn=current_user.fornavn,
+                           etternavn=current_user.etternavn)
 
 
 @app.route("/logout")
@@ -108,14 +129,63 @@ def logout():
     return redirect(url_for('home'))
 
 
+@app.route('/available_books')
+@login_required
+def available_books():
+    # Hent alle bøker som ikke er lånt ut for øyeblikket
+    available_books = Bøker.query.filter(
+        ~Bøker.ISBN.in_(db.session.query(LånteBøker.ISBN).filter_by(Levert=False))).all()
+    return render_template('available_books.html', books=available_books)
+
+
+@app.route('/borrow_book', methods=['POST'])
+@login_required
+def borrow_book():
+    isbn = request.form['isbn']
+    user_id = current_user.id
+    lån_dato = datetime.now().date()
+    retur_dato = lån_dato + timedelta(days=30)
+
+    nytt_lån = LånteBøker(StudentID=user_id, ISBN=isbn, LånDato=lån_dato, ReturDato=retur_dato, Levert=False)
+    db.session.add(nytt_lån)
+    db.session.commit()
+
+    flash('Du har nå lånt boken.')
+    return redirect(url_for('available_books'))
+
+
+@app.route('/available_magazines')
+@login_required
+def available_magazines():
+    available_magazines = Tidsskrifter.query.filter(
+        ~Tidsskrifter.TidsskriftID.in_(db.session.query(LånteTidsskrifter.TidsskriftID).filter_by(Levert=False))).all()
+    return render_template('available_magazines.html', magazines=available_magazines)
+
+
+@app.route('/borrow_magazine', methods=['POST'])
+@login_required
+def borrow_magazine():
+    tidsskrift_id = request.form.get('tidsskriftid')
+    lån_dato = datetime.now().date()
+    retur_dato = lån_dato + timedelta(days=30)  # Anta 30 dagers lån
+
+    nytt_lån = LånteTidsskrifter(StudentID=current_user.id, TidsskriftID=tidsskrift_id, LånDato=lån_dato,
+                                 ReturDato=retur_dato, Levert=False)
+    db.session.add(nytt_lån)
+    db.session.commit()
+
+    flash('Du har nå lånt tidsskriftet.')
+    return redirect(url_for('available_magazines'))
+
+
 class Bøker(db.Model):
     ISBN = db.Column(db.BigInteger, primary_key=True, nullable=False)
     Tittel = db.Column(db.String(100), nullable=False)
     Forfatter = db.Column(db.String(100), nullable=False)
     Sjanger = db.Column(db.String(100), nullable=False)
 
-    # Definerer relasjonen til LånteBøker
     lånte_bøker = relationship("LånteBøker", back_populates="bok")
+    ratings = relationship('BokRating', back_populates='bok')  # Endret her
 
 
 class LånteBøker(db.Model):
@@ -126,8 +196,56 @@ class LånteBøker(db.Model):
     ReturDato = db.Column(db.Date, nullable=True)
     Levert = db.Column(db.Boolean, nullable=False, default=False)
 
-    # Definerer relasjonen til Bøker
     bok = relationship("Bøker", back_populates="lånte_bøker")
+
+
+@app.route('/legg-til-bok', methods=['GET', 'POST'])
+@login_required
+def Legg_til_bok():
+    if current_user.role != 'admin':
+        flash('Ikke tilgang: Kun administratorer har lov til å gjøre dette.', 'error')
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        Tittel = request.form['Tittel']
+        Forfatter = request.form['Forfatter']
+        ISBN = request.form['ISBN']
+        Sjanger = request.form.get('Sjanger')
+        if Sjanger:
+            Sjanger = str(Sjanger)
+
+        new_book = Bøker(Tittel=Tittel, Forfatter=Forfatter, ISBN=ISBN, Sjanger=Sjanger)
+        db.session.add(new_book)
+        db.session.commit()
+        flash('Ny bok lagt til!', 'success')
+        return redirect(url_for('profile'))
+    return render_template('add_books.html')
+
+
+@app.route('/delete-books', methods=['GET'])
+@login_required
+def show_delete_books():
+    if current_user.role != 'admin':
+        flash('Ikke tilgang: Kun administratorer har lov til å gjøre dette.', 'error')
+        return redirect(url_for('index'))
+    books = Bøker.query.all()
+    return render_template('delete_books.html', books=books)
+
+
+@app.route('/delete-book/<int:isbn>', methods=['POST'])
+@login_required
+def delete_book(isbn):
+    if current_user.role != 'admin':
+        flash('Ikke tilgang: Kun administratorer har lov til å gjøre dette.', 'error')
+        return redirect(url_for('index'))
+    book_to_delete = Bøker.query.filter_by(ISBN=isbn).first()
+    if book_to_delete:
+        db.session.delete(book_to_delete)
+        db.session.commit()
+        flash('Boken er slettet.', 'success')
+    else:
+        flash('Boken finnes ikke.', 'error')
+    return redirect(url_for('show_delete_books'))
 
 
 class Tidsskrifter(db.Model):
@@ -136,7 +254,6 @@ class Tidsskrifter(db.Model):
     Utgiver = db.Column(db.String(100), nullable=False)
     Kategori = db.Column(db.String(100), nullable=False)
 
-    # Definerer relasjonen til LånteTidsskrifter
     lånte_tidsskrifter = relationship("LånteTidsskrifter", back_populates="tidsskrift")
 
 
@@ -148,7 +265,6 @@ class LånteTidsskrifter(db.Model):
     ReturDato = db.Column(db.Date, nullable=True)
     Levert = db.Column(db.Boolean, nullable=False, default=False)
 
-    # Definerer relasjonen til Tidsskrifter
     tidsskrift = relationship("Tidsskrifter", back_populates="lånte_tidsskrifter")
 
 
@@ -159,12 +275,12 @@ def innlevering():
         selected_items = request.form.getlist('item_id')
         for item_id in selected_items:
             if item_id.startswith('bok_'):
-                loan = LånteBøker.query.get(item_id.replace('bok_', ''))
+                loan = LånteBøker.query.get(int(item_id.replace('bok_', '')))
             elif item_id.startswith('tidsskrift_'):
-                loan = LånteTidsskrifter.query.get(item_id.replace('tidsskrift_', ''))
+                loan = LånteTidsskrifter.query.get(int(item_id.replace('tidsskrift_', '')))
             if loan:
                 loan.Levert = True
-                loan.ReturDato = datetime.date.today()  # Setter innleveringsdato til dagens dato
+                loan.ReturDato = datetime.now().date()  # Korrigert bruk av datetime
                 db.session.commit()
         return redirect(url_for('profile'))
 
@@ -177,6 +293,27 @@ def innlevering():
     return render_template('innlevering.html', loans_bøker=loans_bøker, loans_tidsskrifter=loans_tidsskrifter,
                            last_delivered_items=last_delivered_items)
 
+
+@app.route('/rate-book/<int:isbn>', methods=['POST'])
+@login_required
+def rate_book(isbn):
+    new_rating = int(request.form.get('rating'))
+    if 1 <= new_rating <= 5:
+        existing_rating = BokRating.query.filter_by(bok_isbn=isbn, student_id=current_user.id).first()
+        if existing_rating:
+            existing_rating.rating = new_rating
+        else:
+            new_book_rating = BokRating(bok_isbn=isbn, student_id=current_user.id, rating=new_rating)
+            db.session.add(new_book_rating)
+        db.session.commit()
+        flash('Din vurdering er registrert.', 'success')
+    else:
+        flash('Vurdering må være et tall mellom 1 og 5.', 'error')
+    return redirect(url_for('available_books'))
+
+
+Bøker.ratings = relationship('BokRating', back_populates='bok')
+User.ratings = relationship('BokRating', back_populates='bruker')
 
 if __name__ == "__main__":
     app.run(debug=True)
